7e7e2fc8b615b5f34a60820e9ca91fca958d906a
Chris Taylor <crntaylor@gmail.com>
2013-06-27 22:25:24 +0100

    Minor change to output of prettyPrint

--- src/Control/Probability/PrettyPrint.hs
+++ src/Control/Probability/PrettyPrint.hs
@@ -11,5 +11,5 @@ prettyPrint (Dist m) = L.intercalate "\n" $ toString len str prb
         str = map (show.fst) m
         prb = map (printf "%6.3f".(100*).snd) m
         len = maximum (map length str)
-        toString n s p = map (\(s,p) -> padTo n s ++ " " ++ p ++ " %") (zip s p)
+        toString n s p = map (\(s,p) -> padTo n s ++ "  " ++ p ++ "%") (zip s p)
         padTo n s      = replicate (n-m) ' ' ++ s where m = length s
